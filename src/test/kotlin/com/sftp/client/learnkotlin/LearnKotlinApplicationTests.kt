51ba9ce14f6dcb7c07feaa5de44f0f84c5b6ca5c
hassanalam <hassan.alam@bisnode.com>
2021-03-04 20:00:40 +0100

    first commit

---
+++ src/test/kotlin/com/sftp/client/learnkotlin/LearnKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sftp.client.learnkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
