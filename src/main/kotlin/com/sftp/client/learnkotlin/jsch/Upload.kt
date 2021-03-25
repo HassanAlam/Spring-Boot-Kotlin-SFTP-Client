93046a3704bebfcb49675b1b0c15ffd343eeb86c
Hassan Alam <hassan@alam.io>
2021-03-25 20:17:18 +0100

    added methds to upload

--- src/main/kotlin/com/sftp/client/learnkotlin/jsch/Upload.kt
+++ src/main/kotlin/com/sftp/client/learnkotlin/jsch/Upload.kt
@@ -1,6 +1,7 @@
 package com.sftp.client.learnkotlin.jsch
 
 import com.sftp.client.learnkotlin.model.LoginSettings
+import java.io.File
 
 class Upload {
 
@@ -11,5 +12,16 @@ class Upload {
     fun uploadFiles(loginSettings: LoginSettings) {
         TODO("Not yet implemented")
     }
+
+    fun deleteFile(delete: String, files: Array<File>, i: Int) {
+        if (delete == "1") {
+            files[i].delete()
+        }
+    }
+
+    fun isForce(force: String): Boolean {
+        return force == "1"
+    }
+
 
 }
