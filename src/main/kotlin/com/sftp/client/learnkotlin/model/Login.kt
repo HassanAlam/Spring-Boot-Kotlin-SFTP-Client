3a67e80bd4f08bd1235cdecd07c7f6b020dad4cd
Hassan Alam <hassan@alam.io>
2021-04-17 13:45:28 +0200

    data class

--- src/main/kotlin/com/sftp/client/learnkotlin/model/Login.kt
+++ src/main/kotlin/com/sftp/client/learnkotlin/model/Login.kt
@@ -1,5 +1,5 @@
 package com.sftp.client.learnkotlin.model
 
-class Login(var login: MutableList<LoginSettings>) {
+data class Login(var login: MutableList<LoginSettings>) {
 
 }
--- src/main/kotlin/com/sftp/client/learnkotlin/model/LoginDetails.kt
+++ src/main/kotlin/com/sftp/client/learnkotlin/model/LoginDetails.kt
@@ -1,6 +1,6 @@
 package com.sftp.client.learnkotlin.model
 
-class LoginDetails(var userName: String = "",var userPassword: String = "",var privateKeyFile: String = "",
+data class LoginDetails(var userName: String = "",var userPassword: String = "",var privateKeyFile: String = "",
                    var privateKeyPassword: String = "",var host: String = "",var port: String = "") {
 
 }
